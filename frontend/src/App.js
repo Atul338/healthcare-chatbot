fa09c56f64571280c05c8b5722f43131b5a49710
Atul Gupta <atulgupta2624@gmail.com>
2026-01-06 22:12:17 +0530

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Healthcare Chatbot</h1>
+      <p>Frontend setup successful ✅</p>
+    </div>
+  );
+}
+
+export default App;
